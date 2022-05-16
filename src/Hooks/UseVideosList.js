5c8dba6ca0aa7080fa2574f712499624fa4a44a2
Tamanna14-cpu <bimurto.bithy@gmail.com>
2022-05-16 14:16:01 +0600

    new

--- src/Hooks/UseVideosList.js
+++ src/Hooks/UseVideosList.js
@@ -1,34 +1,59 @@
-import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
 import { useEffect, useState } from "react";
 
-const UseVideosList = () => {
+const UseVideosList = (page) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [videos, setVideos] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     async function fetchVideo() {
       const db = getDatabase();
       const videosRef = ref(db, "videos");
-      const videoQuery = query(videosRef, orderByKey());
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt("" + page),
+        limitToFirst(8)
+      );
 
       try {
         setError(false);
         setLoading(true);
         const snapshot = await get(videoQuery);
-        console.log(snapshot.val);
         setLoading(false);
         if (snapshot.exists()) {
-          setVideos();
+          setVideos((preVideos) => {
+            return [...preVideos, ...Object.values(snapshot.val())];
+          });
         } else {
+          setHasMore(false);
         }
       } catch (err) {
         console.log(err);
+        setLoading(false);
+        setError(true);
       }
     }
 
     fetchVideo();
-  }, []);
+  }, [page]);
+
+  return {
+    loading,
+    error,
+    videos,
+    hasMore,
+  };
 };
 
 export default UseVideosList;
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,31 +1,27 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import UseVideosList from "../Hooks/UseVideosList";
 import classes from "../styles/Videos.module.css";
 import Video from "./Video";
 
 const Videos = () => {
-    return (
-        <div className={classes.videos}>
-            <Link to="/quiz">
-                <Video />
-            </Link>
-            <Link to="/quiz">
-                <Video />
-            </Link>
-            <Link to="/quiz">
-                <Video />
-            </Link>
-            <Link to="/quiz">
-                <Video />
-            </Link>
-            <Link to="/quiz">
-                <Video />
-            </Link>
-            <Link to="/quiz">
-                <Video />
-            </Link>
+  const { loading, error, videos, hasMore } = UseVideosList(0);
+  const [page, setPage] = useState(1);
 
-        </div>
-    );
+  return (
+    <div className={classes.videos}>
+      {videos.length > 0 &&
+        videos.map((video) => (
+          <Link to="/quiz" key={video.youtubeId}>
+            <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+          </Link>
+        ))}
+
+      {!loading && videos.length === 0 && <div>No Data Found!</div>}
+      {error && <div>Something went wrong!</div>}
+      {loading && <div>Loading...</div>}
+    </div>
+  );
 };
 
 export default Videos;
